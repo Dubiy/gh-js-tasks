5eee76b1b6c0379ed16d0e4b37ed19bed1544431
Gary <netzver@gmail.com>
2016-11-01 22:59:00 +0200

    simple tasks

---
+++ task/task1.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Числа Фиббоначи
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765
+ * https://en.wikipedia.org/wiki/Fibonacci_number
+ *
+ * Знайти суму перших n чисел фібоначі.
+ */
+
+var fibonacciTests = [
+    {
+        parameters: [1],
+        expectedResult: 1
+    },
+    {
+        parameters: [3],
+        expectedResult: 4
+    },
+    {
+        parameters: [5],
+        expectedResult: 12
+    },
+    {
+        parameters: [20],
+        expectedResult: 17710
+    },
+    {
+        parameters: [0],
+        expectedResult: 0
+    }
+];
+
+
+function fibonacci(n) {
+    //TODO
+}
+
+
+tasks.push({
+    title: "Числа Фиббоначи",
+    solution: fibonacci,
+    tests: fibonacciTests
+});
